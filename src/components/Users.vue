948931e2a02e71d4147a579d3f152628638414e5
Matthieu Bessat <spamfree@matthieubessat.fr>
2019-10-25 08:13:02 +0200

    fix(Users): close the modal after destroy

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -329,6 +329,7 @@ export default {
           }
         }
       }).then((response) => {
+        this.destroyUserModal = false
         this.$store.commit('ADD_ALERT', {
           color: 'info',
           text: "User Destroyed!"
