2e03f8dc00e79dfd90ae2ff1ce27a00ff2cd156c
Valentin Derouet <thevalentin61@gmail.com>
2020-05-07 00:05:10 +0200

    feat: a thing ? ok i don't know to do that but, @lefuturiste will help me lol

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
   API_ENDPOINT: '"http://localhost:8000"',
   COOKIE_DOMAIN: '"localhost"'
 })
